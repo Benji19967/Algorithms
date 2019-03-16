65b43fc87601c78b251c73e0c3518dd18063687f
Benjamin Labrecque <35387157+Benji19967@users.noreply.github.com>
2019-03-16 18:15:27 -0400

    Binomial Coefficients Bottom-up DP

--- binomial_coeffcients.cpp
+++ binomial_coeffcients.cpp
@@ -17,20 +17,21 @@ int main() {
 
     t.push_back(vi(1, 1));
     t.push_back(vi(2, 1));
+    cout << 1 << endl;
+    cout << 1 << " " << 1 << endl;
 
     for(int i = 2; i < n; i++) {
         t.push_back(vi(i+1));
         t[i][0] = 1;
         t[i][i] = 1;
+        cout << 1 << " ";
         for(int j = 1; j < i; j++) {
             t[i][j] = t[i-1][j-1] + t[i-1][j];
-        }
-    }
-
-    for(int i = 0; i < t.size(); i++) {
-        for(int j = 0; j < t[i].size(); j++) {
             cout << t[i][j] << " ";
         }
+        cout << 1;
         cout << endl;
+        t[i-1].clear();
+        t[i-1].shrink_to_fit();
     }
 }
