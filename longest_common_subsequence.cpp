126fd5692359f0303fe17e8b859341e97e713bc8
Benjamin Labrecque <35387157+Benji19967@users.noreply.github.com>
2019-03-27 12:49:55 -0400

    Longest Common Subsequence DP

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -11,7 +11,7 @@ int LCS(string &X, string &Y) {
 	int n = X.length();
 	int m = Y.length();
 
-    int dp[2][m+1];
+	int dp[2][m+1];
 	bool curr;
 
 	for(int i = 0; i <= n; i++) {
@@ -20,16 +20,15 @@ int LCS(string &X, string &Y) {
 	
 		for(int j = 0; j <= m; j++) {
 			if(i == 0 || j == 0) {
-                dp[curr][j] = 0;
-            }
-            else if(X[i-1] == Y[j-1]) {
+				dp[curr][j] = 0;	
+			}
+			else if(X[i-1] == Y[j-1]) {
 				dp[curr][j] = dp[1-curr][j-1] + 1;
 			}
 			else {
 				dp[curr][j] = max(dp[1-curr][j], dp[curr][j-1]);
 			}
 		}
-
 	}
 	return dp[curr][m];
 }
